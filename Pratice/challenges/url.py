1c6bbb57e9d1a399f56fc31413dd2b814995b0d8
squirrelli16 <94501455+squirrelli16@users.noreply.github.com>
2024-06-14 16:53:38 +0700

    finish chapter 3: url and view
    learning about return dynamic views

--- Pratice/challenges/url.py
+++ Pratice/challenges/url.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 urlpatterns=[
         path("<int:month>",views.monthly_by_number),
-        path("<str:month>", views.monthly),
+        path("<str:month>", views.monthly,name="monthly_by_month"),
+        path("", views.Listmonth,name="Listmonth"),
 ]
--- Pratice/challenges/views.py
+++ Pratice/challenges/views.py
@@ -1,7 +1,7 @@
 from django.middleware import http
 from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 from django.shortcuts import render
-
+from django.urls import reverse
 m={
     "january":"january",
     "february":"february",
@@ -24,10 +24,21 @@ def monthly_by_number(request,month):
         return HttpResponseNotFound("this month does not exist")
     else:
         forward_month= tranfer[month-1]
-    return HttpResponseRedirect("/challenges/"+forward_month)
+    redirect_path= reverse("monthly_by_month",args=[forward_month])
+    return HttpResponseRedirect(redirect_path)
 def monthly(request,month):
     try:
         challenge= m[month]
-        return HttpResponse(challenge)
+        response_data=f"<h1>{challenge}</h1>"
+        return HttpResponse(response_data)
     except:
-        return HttpResponseNotFound("this month does not exist")
+        return HttpResponseNotFound("<h1>this month does not exist</h1>")
+def Listmonth(request):
+    list_month=""
+    month_list=list(m.keys())
+    for month in month_list:
+        capitalize_month=month.capitalize()
+        month_path=reverse("monthly_by_month",args=[month])
+        list_month+= f"<li><a href=\"{month_path}\">{capitalize_month}</li>"
+    response_data= list_month
+    return HttpResponse(response_data)
